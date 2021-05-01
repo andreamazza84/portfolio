2a94be94d7c3da109268b6a32641ae465b75b21f
andreamazza84 <andrea.mazza84@gmail.com>
2021-05-01 13:14:08 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class="neon-font">
+      <router-link to="/" class="btn">Home</router-link> |
+      <router-link to="/about" class="btn">About</router-link>
     </div>
     <router-view/>
+
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
+@import "~@/scss/components/Navbar.scss";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
